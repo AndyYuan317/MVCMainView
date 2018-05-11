9b2374bbea126e9bded1299b54ad33a1775bad1b
AndyYuan317 <1065416114@qq.com>
2018-05-11 09:37:14 +0800

    加入运行图片

--- app/src/main/java/com/example/mainview/MallApplication.java
+++ app/src/main/java/com/example/mainview/MallApplication.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 
@@ -16,7 +15,7 @@ public class MallApplication extends AppCompatActivity {
     public static boolean hasPromotion = true;
     public static Activity activity;
     private static MallApplication application;
-    private static Handler handler;
+    //private static Handler handler;
     private static int mainThreadId;
     private Context context;
     private static ProgressDialog progressDialog;
@@ -26,7 +25,7 @@ public class MallApplication extends AppCompatActivity {
         super.onCreate(savedInstance);
         application = this;
         context = getApplicationContext();
-        handler = new Handler();
+       // handler = new Handler();
         mainThreadId = android.os.Process.myPid();
 
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
